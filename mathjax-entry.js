80afc9d7d59a0df7d7e777ec010c82f1ea7a714f
Sebastian Rettig <sr258@users.noreply.github.com>
2017-09-28 21:42:39 +0200

    now fully working + documentation

--- mathjax-entry.js
+++ mathjax-entry.js
@@ -3,8 +3,7 @@ var H5P = H5P || {};
 /**
  * MathJax library for H5P.
  *
- * This is a utility library, which does not implement attach. I.e, it has to bee actively used by
- * other libraries
+ * This is a utility library, which does not implement attach. I.e, it has to be actively used by other libraries
  * @module
  */
 H5P.MathJax = (function ($) {
@@ -16,7 +15,21 @@ H5P.MathJax = (function ($) {
    */
   function MathJaxLib() { }
 
+  /**
+   * The number of milliseconds which MathJax requests should be throttled.
+   */
+  var throttleTime = 5;
+
+  /**
+   * The id of the currently running timer used for throtteling MathJax requests.
+   */
+  var runningThrottleTimeoutId = null;    
+
   /* Private functions */
+  
+  /**
+   * Configures MathJax
+   */
   var configureMathJax = function () {
     window.MathJax = {
       showProcessingMessages: false,
@@ -24,6 +37,10 @@ H5P.MathJax = (function ($) {
     };
   }
 
+  /**
+   * Returns the root path of this library.
+   * @returns string
+   */
   var getCurrentPath = function () {
     var jsFileLocation = $('script[src*=mathjax-entry]').attr('src');
     if (!jsFileLocation) {
@@ -34,6 +51,10 @@ H5P.MathJax = (function ($) {
     return jsFileLocation;
   }
 
+  /**
+   * Loads MathJax.
+   * @param {string} path The path to the root directory of this library.
+   */  
   var loadMathJax = function (path) {
     var script = document.createElement("script");
     script.type = "text/javascript";
@@ -41,29 +62,39 @@ H5P.MathJax = (function ($) {
     document.getElementsByTagName("head")[0].appendChild(script);
   }
 
+  /**
+   * Queues a request at MathJax that the node should be updated.
+   * @param {Node} node The nodes whose descendants should be updated.
+   */
   var doJax = function (node) {
     if (MathJax.Hub)
       MathJax.Hub.Queue(['Typeset', MathJax.Hub, node]);
   };
+  
+  /**
+   * Updates the container's descendants but throttles the updates to reduce CPU load.
+   * @param  {Node} container The node whose descendants should be updated.
+   */
+  var throttledJaxUpdate = function (container) {
+    if (!runningThrottleTimeoutId) {
+      runningThrottleTimeoutId = setTimeout(function () {            
+          doJax(container);            
+        runningThrottleTimeoutId = null;
+      }, throttleTime); 
+    }
+  };
 
+  /**
+   * Sets up an observer that calls MathJax when changes were made to the DOM.
+   * @param  {Node} container The root node to whose descendants changes should be observed.
+   */
   var setupObserver = function (container) {
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
     if (MutationObserver) {
-      var running = false;
-      var limitedResize = function (nodes) {
-        if (!running) {
-          running = setTimeout(function () {
-            
-              doJax(container);            
-            running = null;
-          }, 500); // 2 fps cap
-        }
-      };
-
       var observer = new MutationObserver(function (mutations) {
         for (var i = 0; i < mutations.length; i++) {
           if (mutations[i].addedNodes.length) {
-            limitedResize();
+            throttledJaxUpdate(container);
             return;
           }
         }
@@ -78,9 +109,8 @@ H5P.MathJax = (function ($) {
   /* Public static functions */
 
   /**
-   * Load the MathJax library
-   * @method H5P.MathJax.load
-   * @return 
+   * Loads the MathJax library
+   * @param  {Node} container The node whose descendants should be parsed with MathJax.
    */
   MathJaxLib.load = function (container) {
     var currentPath = getCurrentPath();
