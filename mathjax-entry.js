554511e97f0d6cb33d882ae541b7cc0739eb4cbe
Sebastian Rettig <sr258@users.noreply.github.com>
2017-09-28 18:56:28 +0200

    added MutationObserver (not functional)

--- mathjax-entry.js
+++ mathjax-entry.js
@@ -34,11 +34,39 @@ H5P.MathJax = (function ($) {
     return jsFileLocation;
   }
 
-  var loadMathJax = function(path) {
+  var loadMathJax = function (path) {
     var script = document.createElement("script");
     script.type = "text/javascript";
     script.src = path + "js/MathJax.js?config=TeX-AMS_CHTML";
     document.getElementsByTagName("head")[0].appendChild(script);
+  }
+
+  var doJax = function (node) {
+    if (MathJax.Hub)
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub, node]);
+  };
+
+  var setupObserver = function (container) {
+
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    if (MutationObserver) {
+
+      var observer = new MutationObserver(function (mutations) {
+        for (var i = 0; i < mutations.length; i++) {
+          if (mutations[i].addedNodes.length) {
+            for (var x = 0; x < mutations[i].addedNodes.length; x++) {
+              $('*', mutations[i].addedNodes[x]).each(function (j, m) {
+                doJax(m.parentNode);
+              });
+            }
+          }
+        }
+      });
+      observer.observe(container, {
+        childList: true,
+        subtree: true
+      });
+    }
   }
 
   /* Public static functions */
@@ -48,13 +76,14 @@ H5P.MathJax = (function ($) {
    * @method H5P.MathJax.load
    * @return 
    */
-  MathJax.load = function () {
+  MathJax.load = function (container) {
     var currentPath = getCurrentPath();
     if (!currentPath)
       return;
 
-    configureMathJax();    
+    configureMathJax();
     loadMathJax(currentPath);
+    setupObserver(container);
   };
 
   return MathJax;
